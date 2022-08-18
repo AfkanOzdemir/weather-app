87904e0cb7f7b33c2475e2a93ee2004585b2e33d
Afkan <110481565+AfkanOzdemir@users.noreply.github.com>
2022-08-18 16:38:42 +0300

    Changing font and icons

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,20 +6,145 @@ import logo from "../images/logo/white.png";
 import test from "../images/weatherMoods/1.png";
 import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
+import React, { useState } from "react";
 import {
-  faCloudSun,
-  faCloudRain,
-  faRainbow,
-  faCloud,
-  faCloudBolt,
-  faSnowflake,
-  faCloudMoon,
-  faWind,
-  faTornado,
-} from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+  WiCloud,
+  WiCloudyGusts,
+  WiDayCloudyGusts,
+  WiDayHail,
+  WiDayShowers,
+  WiDaySnowThunderstorm,
+  WiDaySunnyOvercast,
+  WiDaySunny,
+  WiDayWindy,
+  WiDayCloudy,
+  WiHail,
+  WiNightAltRainMix,
+} from "react-icons/wi";
+import "swiper/css";
+
 export default function Home() {
+  const [changePanel, setPanel] = useState(0);
+  let todayPanel = () => {
+    return (
+      <Swiper
+        spaceBetween={40}
+        slidesPerView={4}
+        freeMode={true}
+        className={panelStyle.mySwiper}
+      >
+        <SwiperSlide>
+          <div className={panelStyle.weather}>
+            <p>Now</p>
+            <WiDayCloudy className={panelStyle.icon} />
+            <h3>18°</h3>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={panelStyle.weather}>
+            <p>7:00</p>
+            <WiDayCloudy className={panelStyle.icon} />
+            <h3>27°</h3>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={panelStyle.weather}>
+            <p>8:00</p>
+            <WiDaySunny className={panelStyle.icon} />
+            <h3>24°</h3>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={panelStyle.weather}>
+            <p>9:00</p>
+            <WiDaySunny className={panelStyle.icon} />
+            <h3>18°</h3>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={panelStyle.weather}>
+            <p>10:00</p>
+            <WiHail className={panelStyle.icon} />
+            <h3>16°</h3>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={panelStyle.weather}>
+            <p>11:00</p>
+            <WiDayShowers className={panelStyle.icon} />
+            <h3>18°</h3>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={panelStyle.weather}>
+            <p>12:00</p>
+            <WiDayShowers className={panelStyle.icon} />
+            <h3>20°</h3>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    );
+  };
+  let weekPanel = () => {
+    return (
+      <Swiper
+        spaceBetween={40}
+        slidesPerView={4}
+        freeMode={true}
+        className={panelStyle.mySwiper}
+      >
+        <SwiperSlide>
+          <div className={panelStyle.weather}>
+            <p>Mon</p>
+            <WiDayCloudy className={panelStyle.icon} />
+            <h3>18°</h3>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={panelStyle.weather}>
+            <p>Tue</p>
+            <WiCloud className={panelStyle.icon} />
+            <h3>27°</h3>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={panelStyle.weather}>
+            <p>Wed</p>
+            <WiDayHail className={panelStyle.icon} />
+            <h3>24°</h3>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={panelStyle.weather}>
+            <p>Thu</p>
+            <WiNightAltRainMix className={panelStyle.icon} />
+            <h3>18°</h3>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={panelStyle.weather}>
+            <p>Fri</p>
+            <WiCloudyGusts className={panelStyle.icon} />
+            <h3>16°</h3>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={panelStyle.weather}>
+            <p>Sat</p>
+            <WiDayWindy className={panelStyle.icon} />
+            <h3>18°</h3>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={panelStyle.weather}>
+            <p>Sun</p>
+            <WiDaySnowThunderstorm className={panelStyle.icon} />
+            <h3>20°</h3>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    );
+  };
   return (
     <div className={styles.container}>
       <Head>
@@ -71,7 +196,7 @@ export default function Home() {
               </div>
             </div>
             <div className={styles.timer}>
-              <div className={styles.timerLine}></div>
+              <div className={styles.timerCurve}></div>
             </div>
           </div>
           <div className={panelStyle.weatherPanel}>
@@ -83,89 +208,23 @@ export default function Home() {
             </section>
             <div className={panelStyle.panelContainer}>
               <div className={panelStyle.panelSelection}>
-                <p>Today</p>
-                <p>Week</p>
+                <p
+                  onClick={() => {
+                    setPanel(0);
+                  }}
+                >
+                  Today
+                </p>
+                <p
+                  onClick={() => {
+                    setPanel(1);
+                  }}
+                >
+                  Week
+                </p>
               </div>
               <div className={panelStyle.weatherMood}>
-                <Swiper
-                  spaceBetween={40}
-                  slidesPerView={4}
-                  freeMode={true}
-                  onSlideChange={() => console.log("slide change")}
-                  onSwiper={(swiper) => console.log(swiper)}
-                  className={panelStyle.mySwiper}
-                >
-                  <SwiperSlide>
-                    <div className={panelStyle.weather}>
-                      <p>Now</p>
-                      <FontAwesomeIcon
-                        icon={faCloudSun}
-                        className={panelStyle.icon}
-                      />
-                      <h3>18°</h3>
-                    </div>
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <div className={panelStyle.weather}>
-                      <p>7:00</p>
-                      <FontAwesomeIcon
-                        icon={faCloudSun}
-                        className={panelStyle.icon}
-                      />
-                      <h3>27°</h3>
-                    </div>
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <div className={panelStyle.weather}>
-                      <p>8:00</p>
-                      <FontAwesomeIcon
-                        icon={faCloudSun}
-                        className={panelStyle.icon}
-                      />
-                      <h3>24°</h3>
-                    </div>
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <div className={panelStyle.weather}>
-                      <p>9:00</p>
-                      <FontAwesomeIcon
-                        icon={faCloudRain}
-                        className={panelStyle.icon}
-                      />
-                      <h3>18°</h3>
-                    </div>
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <div className={panelStyle.weather}>
-                      <p>10:00</p>
-                      <FontAwesomeIcon
-                        icon={faSnowflake}
-                        className={panelStyle.icon}
-                      />
-                      <h3>16°</h3>
-                    </div>
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <div className={panelStyle.weather}>
-                      <p>11:00</p>
-                      <FontAwesomeIcon
-                        icon={faRainbow}
-                        className={panelStyle.icon}
-                      />
-                      <h3>18°</h3>
-                    </div>
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <div className={panelStyle.weather}>
-                      <p>12:00</p>
-                      <FontAwesomeIcon
-                        icon={faWind}
-                        className={panelStyle.icon}
-                      />
-                      <h3>20°</h3>
-                    </div>
-                  </SwiperSlide>
-                </Swiper>
+                {changePanel == 0 ? todayPanel() : weekPanel()}
               </div>
             </div>
           </div>
