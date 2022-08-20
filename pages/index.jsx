68034b4aec8a62b4e9afe2f61957e53714a677cc
Afkan <110481565+AfkanOzdemir@users.noreply.github.com>
2022-08-20 23:55:04 +0300

    added image, added new lib, updated index & css

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,10 +3,10 @@ import styles from "../styles/weather.module.css";
 import headerStyle from "../styles/header.module.css";
 import panelStyle from "../styles/panel.module.css";
 import logo from "../images/logo/white.png";
-import test from "../images/weatherMoods/1.png";
 import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import moment from "moment-timezone";
 import {
   WiCloud,
   WiCloudyGusts,
@@ -20,216 +20,198 @@ import {
   WiDayCloudy,
   WiHail,
   WiNightAltRainMix,
+  WiMoonAltWaxingGibbous6,
 } from "react-icons/wi";
 import "swiper/css";
 
 export default function Home() {
-  const [changePanel, setPanel] = useState(0);
+  const [weather, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+  let [changeLineLocation, setLineLocation] = useState(true);
+  let [changePanel, setPanel] = useState(0);
+  const fetchData = async () => {
+    await fetch(
+      "https://api.weatherapi.com/v1/forecast.json?key=3ffb882b90a94dc9bba175653221808&q=Istanbul&days=7&aqi=yes&alerts=yes"
+    )
+      .then(async (response) => {
+        return response.json();
+      })
+      .then(async (data) => {
+        setUsers(data);
+        setLoading(true);
+        console.log(data);
+      });
+  };
+  console.log(moment().format("DD MM YYYY"));
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   let todayPanel = () => {
     return (
       <Swiper
-        spaceBetween={40}
+        spaceBetween={30}
         slidesPerView={4}
         freeMode={true}
         className={panelStyle.mySwiper}
       >
-        <SwiperSlide>
-          <div className={panelStyle.weather}>
-            <p>Now</p>
-            <WiDayCloudy className={panelStyle.icon} />
-            <h3>18°</h3>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className={panelStyle.weather}>
-            <p>7:00</p>
-            <WiDayCloudy className={panelStyle.icon} />
-            <h3>27°</h3>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className={panelStyle.weather}>
-            <p>8:00</p>
-            <WiDaySunny className={panelStyle.icon} />
-            <h3>24°</h3>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className={panelStyle.weather}>
-            <p>9:00</p>
-            <WiDaySunny className={panelStyle.icon} />
-            <h3>18°</h3>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className={panelStyle.weather}>
-            <p>10:00</p>
-            <WiHail className={panelStyle.icon} />
-            <h3>16°</h3>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className={panelStyle.weather}>
-            <p>11:00</p>
-            <WiDayShowers className={panelStyle.icon} />
-            <h3>18°</h3>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className={panelStyle.weather}>
-            <p>12:00</p>
-            <WiDayShowers className={panelStyle.icon} />
-            <h3>20°</h3>
-          </div>
-        </SwiperSlide>
+        {weather.forecast.forecastday[0].hour.map((value, index) => {
+          return (
+            <SwiperSlide key={index}>
+              <div className={panelStyle.weather}>
+                <p>{moment.unix(value.time_epoch).format("LT")}</p>
+                <div className={panelStyle.imageContainer}>
+                  {
+                    <Image
+                      src={"https:" + value.condition.icon}
+                      alt="icon"
+                      width={64}
+                      height={64}
+                      className={panelStyle.icon}
+                    />
+                  }
+                </div>
+                <h3>{value.temp_c}&deg;</h3>
+              </div>
+            </SwiperSlide>
+          );
+        })}
       </Swiper>
     );
   };
   let weekPanel = () => {
     return (
       <Swiper
-        spaceBetween={40}
+        spaceBetween={30}
         slidesPerView={4}
         freeMode={true}
         className={panelStyle.mySwiper}
       >
-        <SwiperSlide>
-          <div className={panelStyle.weather}>
-            <p>Mon</p>
-            <WiDayCloudy className={panelStyle.icon} />
-            <h3>18°</h3>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className={panelStyle.weather}>
-            <p>Tue</p>
-            <WiCloud className={panelStyle.icon} />
-            <h3>27°</h3>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className={panelStyle.weather}>
-            <p>Wed</p>
-            <WiDayHail className={panelStyle.icon} />
-            <h3>24°</h3>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className={panelStyle.weather}>
-            <p>Thu</p>
-            <WiNightAltRainMix className={panelStyle.icon} />
-            <h3>18°</h3>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className={panelStyle.weather}>
-            <p>Fri</p>
-            <WiCloudyGusts className={panelStyle.icon} />
-            <h3>16°</h3>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className={panelStyle.weather}>
-            <p>Sat</p>
-            <WiDayWindy className={panelStyle.icon} />
-            <h3>18°</h3>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className={panelStyle.weather}>
-            <p>Sun</p>
-            <WiDaySnowThunderstorm className={panelStyle.icon} />
-            <h3>20°</h3>
-          </div>
-        </SwiperSlide>
+        {weather.forecast.forecastday.map((value, index) => {
+          return (
+            <SwiperSlide key={index}>
+              <div className={panelStyle.weather}>
+                <p>{moment.unix(value.date_epoch).format("ddd")}</p>
+                <div className={panelStyle.imageContainer}>
+                  {
+                    <Image
+                      src={"https:" + value.day.condition.icon}
+                      alt="icon"
+                      width={64}
+                      height={64}
+                      className={panelStyle.icon}
+                    />
+                  }
+                </div>
+                <h3>{value.day.avgtemp_c}&deg;</h3>
+              </div>
+            </SwiperSlide>
+          );
+        })}
       </Swiper>
     );
   };
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Weather App</title>
-        <meta name="description" content="Created By Afkan Ozdemir" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+  if (loading) {
+    return (
+      <div className={styles.container}>
+        <Head>
+          <title>Weather App</title>
+          <meta name="description" content="Created By Afkan Ozdemir" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <main className={styles.main}>
-        <div className={styles.screen}>
-          <div className={headerStyle.header}>
-            <span className={headerStyle.logoContainer}>
-              <Image
-                src={logo}
-                alt="logo"
-                width={32}
-                height={24}
-                className={headerStyle.logo}
-              />
-            </span>
-            <span className={headerStyle.menuContainer}>
-              <span>
-                <div></div>
-                <div></div>
-                <div></div>
+        <main className={styles.main}>
+          <div className={styles.screen}>
+            <div className={headerStyle.header}>
+              <span className={headerStyle.logoContainer}>
+                <Image
+                  src={logo}
+                  alt="logo"
+                  width={32}
+                  height={24}
+                  className={headerStyle.icon}
+                />
               </span>
-            </span>
-          </div>
-          <div className={styles.weatherContent}>
-            <h3 className={styles.location}>Kiev</h3>
-            <h1 className={styles.degree}>18°</h1>
-            <div className={styles.weatherIcons}>
-              <Image
-                src={test}
-                alt="logo"
-                width={155}
-                height={116}
-                className={headerStyle.logo}
-              />
+              <span className={headerStyle.menuContainer}>
+                <span>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                </span>
+              </span>
             </div>
-            <div className={styles.sunTime}>
-              <div className={styles.sunRiseContainer}>
-                <p>Sunrise</p>
-                <p>06:05</p>
+            <div className={styles.weatherContent}>
+              <h3 className={styles.location}>{weather.location.name}</h3>
+              <h1 className={styles.degree}>{weather.current.temp_c}&deg;</h1>
+              {/* weather.current.temp_c weather.location.name */}
+              <div className={styles.weatherIcons}>
+                {/* "https:" + weather.forecast.forecastday[0].day.condition.icon */}
+                {
+                  <Image
+                    src={"https:" + weather.current.condition.icon}
+                    alt="weatherMood"
+                    width={128}
+                    height={128}
+                    className={headerStyle.icon}
+                  />
+                }
               </div>
-              <div className={styles.sunsetContainer}>
-                <p>Sunset</p>
-                <p>18:45</p>
+              <div className={styles.sunTime}>
+                <div className={styles.sunRiseContainer}>
+                  <p>Sunrise</p>
+                  <p>{weather.forecast.forecastday[0].astro.sunrise}</p>
+                </div>
+                <div className={styles.sunsetContainer}>
+                  <p>Sunset</p>
+                  <p>{weather.forecast.forecastday[0].astro.sunset}</p>
+                </div>
+              </div>
+              <div className={styles.timer}>
+                <div className={styles.timerCurve}></div>
               </div>
             </div>
-            <div className={styles.timer}>
-              <div className={styles.timerCurve}></div>
-            </div>
-          </div>
-          <div className={panelStyle.weatherPanel}>
-            <section>
-              <div className={panelStyle.wave1}></div>
-              <div className={panelStyle.wave2}></div>
-              <div className={panelStyle.wave3}></div>
-              <div className={panelStyle.wave4}></div>
-            </section>
-            <div className={panelStyle.panelContainer}>
-              <div className={panelStyle.panelSelection}>
-                <p
-                  onClick={() => {
-                    setPanel(0);
-                  }}
+            <div className={panelStyle.weatherPanel}>
+              <section>
+                <div className={panelStyle.wave1}></div>
+                <div className={panelStyle.wave2}></div>
+                <div className={panelStyle.wave3}></div>
+                <div className={panelStyle.wave4}></div>
+              </section>
+              <div className={panelStyle.panelContainer}>
+                <div
+                  className={
+                    changeLineLocation
+                      ? panelStyle.lineDefault
+                      : panelStyle.lineChanged
+                  }
                 >
-                  Today
-                </p>
-                <p
-                  onClick={() => {
-                    setPanel(1);
-                  }}
-                >
-                  Week
-                </p>
-              </div>
-              <div className={panelStyle.weatherMood}>
-                {changePanel == 0 ? todayPanel() : weekPanel()}
+                  <p
+                    onClick={() => {
+                      setPanel(0);
+                      setLineLocation(true);
+                    }}
+                  >
+                    Today
+                  </p>
+                  <p
+                    onClick={() => {
+                      setPanel(1);
+                      setLineLocation(false);
+                    }}
+                  >
+                    Week
+                  </p>
+                </div>
+                <div className={panelStyle.weatherMood}>
+                  {changePanel == 0 ? todayPanel() : weekPanel()}
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </main>
-    </div>
-  );
+        </main>
+      </div>
+    );
+  } else {
+    return "loading";
+  }
 }
